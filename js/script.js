c3b97ee020f76aa821dc38955caa1064495f59d7
olemeha <77707992+olemeha@users.noreply.github.com>
2021-10-19 13:43:10 -0500

    fixed errors and added an event listener when submit button is clicked. Fields are checked when the button is clicked.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var firstName = document.getElementByID("first_name_field").value;
-var lastName = document.getElementByID("last_name_field").value;
-var userName = document.getElementByID("username_field").value;
-var email = document.getElementByID("email_field").value;
-var phoneNumber = document.getElementByID("phone_field").value;
-var dateBirth = document.getElementByID("date_field").value;
+var firstName = document.getElementById("first_name_field").value;
+var lastName = document.getElementById("last_name_field").value;
+var userName = document.getElementById("username_field").value;
+var email = document.getElementById("email_field").value;
+var phoneNumber = document.getElementById("phone_field").value;
+var dateBirth = document.getElementById("date_field").value;
 
 
 function jsSwitch(){
@@ -15,10 +15,10 @@ function checkFields(){
 		alert("All fields should be filled in! Please check and fill in the missing fields.");
 		return false;
 	} 
-	if(!document.getElementByID("age-verification").checked){
+	if(!document.getElementById("age-verification").checked){
 		alert("You Must Be 18 or Over!");
 		return false;
-	}else if(!document.getElementByID("shooter_games").checked && !document.getElementByID("puzzle_games").checked && !document.getElementByID("sports_games").checked && !document.getElementByID("racing_games").checked){
+	}else if(!document.getElementById("shooter_games").checked && !document.getElementById("puzzle_games").checked && !document.getElementById("sports_games").checked && !document.getElementById("racing_games").checked){
 		alert("At Least One Should be Checked!");
 		return false;
 	}
@@ -28,3 +28,13 @@ function checkFields(){
 }
 
 window.addEventListener('load',jschg);
+document.getElementById("submit"). addEventListener("click", 
+	function(evt){
+		evt.preventDefault();
+
+		if(checkFields()){
+			alert("Congratulations on Completing Your Sign Up! We will send you a confirmation via email: " + email +  " and phone: " + phoneNumber);
+		}
+		input.value = '';
+	}	
+);
