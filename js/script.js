af785d909fd7e8740abc3c561fc9ccd42542ce21
olemeha <77707992+olemeha@users.noreply.github.com>
2021-10-19 14:16:59 -0500

    input a div with an id of errorMsg, to show an alert if fields are not filled in.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-var firstName = document.getElementById("first_name_field").value;
-var lastName = document.getElementById("last_name_field").value;
-var userName = document.getElementById("username_field").value;
-var email = document.getElementById("email_field").value;
-var phoneNumber = document.getElementById("phone_field").value;
-var dateBirth = document.getElementById("date_field").value;
-
 
 
 function checkFields(){
-	if(firstName == null || lastName == null || userName == null || email == null || phoneNumber == null || dateBirth == null){
-		alert("All fields should be filled in! Please check and fill in the missing fields.");
+	var firstName = document.getElementById("first_name_field").value;
+	var lastName = document.getElementById("last_name_field").value;
+	var userName = document.getElementById("username_field").value;
+	var email = document.getElementById("email_field").value;
+	var phoneNumber = document.getElementById("phone_field").value;
+	var dateBirth = document.getElementById("date_field").value;
+
+
+	if(firstName == "" || lastName == "" || userName == "" || email == ""|| phoneNumber == "" || dateBirth == ""){
+		document.getElementById("errorMsg").innerHTML = "All fields should be filled in! Please check and fill in the missing fields.";
 		return false;
 	} 
 	if(!document.getElementById("age-verification").checked){
-		alert("You Must Be 18 or Over!");
+		document.getElementById("errorMsg").innerHTML = "You Must Be 18 or Over!";
 		return false;
 	}else if(!document.getElementById("shooter_games").checked && !document.getElementById("puzzle_games").checked && !document.getElementById("sports_games").checked && !document.getElementById("racing_games").checked){
 		alert("At Least One Should be Checked!");
