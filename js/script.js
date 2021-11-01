3665eba5dad0162d99855c0a0c01d7c611f10fee
olemeha <77707992+olemeha@users.noreply.github.com>
2021-11-01 16:46:45 -0500

    fix js to check if the fields are filled out.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function(){
-    document.getElementById('submit').addEventListener("submit", function(){
+    document.getElementById('submit').addEventListener("click", function(){
         checkFields();
     })
 });
@@ -20,11 +20,14 @@ function checkFields(){
 		alert("You Must Be 18 or Over!");
 		return false;
 	}else if(!document.getElementById("shooter_games").checked && !document.getElementById("puzzle_games").checked && !document.getElementById("sports_games").checked && !document.getElementById("racing_games").checked){
-		alert("At Least One Should be Checked!");
+		alert("At least one game genre should be checked!");
 		return false;
 	}
 	else{
 		alert("Sign Up Complete!");
+		alert("Your next game to test will be sent to you email: " + email);
 		return true;
 	}
 }
+
+
